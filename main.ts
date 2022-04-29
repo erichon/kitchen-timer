c46f93acbd625f4c90f6c3d503ddb3c06d220e10
erichon <erichon18@gmail.com>
2022-04-29 21:07:34 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,6 @@
-function _60min () {
-    while (count <= 3) {
-        basic.pause(3000)
-        count += 1
-    }
-    flashingP8()
-    count = 0
-}
-function _30min () {
-    while (count <= 1) {
-        basic.pause(3000)
-        count += 1
-    }
-    flashingP8()
-    count = 0
-}
-function _15min () {
-    while (count == 0) {
-        basic.pause(3000)
+function fourth_Time () {
+    while (count <= fourthTime) {
+        basic.pause(1000)
         count += 1
     }
     flashingP8()
@@ -28,9 +12,17 @@ function clearALL () {
     pins.digitalWritePin(DigitalPin.P7, 0)
     pins.digitalWritePin(DigitalPin.P8, 0)
 }
-function _45min () {
-    while (count <= 2) {
-        basic.pause(3000)
+function first_Time () {
+    while (count <= firstTime) {
+        basic.pause(1000)
+        count += 1
+    }
+    flashingP8()
+    count = 0
+}
+function second_Time () {
+    while (count <= secondTime) {
+        basic.pause(1000)
         count += 1
     }
     flashingP8()
@@ -40,50 +32,73 @@ radio.onReceivedValue(function (name, value) {
     if (name == "alarm") {
         if (value == 0) {
             stop_alarm = 1
+            pins.digitalWritePin(DigitalPin.P16, 0)
+            clearALL()
         }
     }
 })
+function third_Time () {
+    while (count <= thirdTime) {
+        basic.pause(1000)
+        count += 1
+    }
+    flashingP8()
+    count = 0
+}
 function flashingP8 () {
     pins.digitalWritePin(DigitalPin.P16, 1)
-    radio.sendValue("alarm", 1)
-    while (pins.digitalReadPin(DigitalPin.P0) == 1 || stop_alarm == 1) {
-        pins.digitalWritePin(DigitalPin.P8, 1)
-        basic.pause(100)
-        pins.digitalWritePin(DigitalPin.P8, 0)
-        basic.pause(100)
+    radio.sendValue("ralarm", 1)
+    while (pins.digitalReadPin(DigitalPin.P0) == 1 && stop_alarm == 0) {
+        if (stop_alarm == 0) {
+            pins.digitalWritePin(DigitalPin.P8, 1)
+            basic.pause(100)
+            pins.digitalWritePin(DigitalPin.P8, 0)
+            basic.pause(100)
+        } else {
+            pins.digitalWritePin(DigitalPin.P8, 0)
+        }
     }
+    radio.sendValue("ralarm", 0)
     pins.digitalWritePin(DigitalPin.P16, 0)
     basic.pause(500)
     stop_alarm = 0
     clearALL()
 }
+let fourthTime = 0
+let thirdTime = 0
+let secondTime = 0
+let firstTime = 0
 let count = 0
 let stop_alarm = 0
 radio.setGroup(99)
 stop_alarm = 0
-clearALL()
 count = 0
 pins.digitalWritePin(DigitalPin.P16, 0)
+clearALL()
 led.enable(false)
+firstTime = 10
+secondTime = 15
+thirdTime = 20
+fourthTime = 25
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P3) == 0) {
         clearALL()
         pins.digitalWritePin(DigitalPin.P4, 1)
-        _15min()
+        first_Time()
     }
     if (pins.digitalReadPin(DigitalPin.P2) == 0) {
         clearALL()
         pins.digitalWritePin(DigitalPin.P6, 1)
-        _30min()
+        second_Time()
     }
     if (pins.digitalReadPin(DigitalPin.P1) == 0) {
         clearALL()
         pins.digitalWritePin(DigitalPin.P7, 1)
-        _45min()
+        third_Time()
     }
     if (pins.digitalReadPin(DigitalPin.P0) == 0) {
         clearALL()
         pins.digitalWritePin(DigitalPin.P8, 1)
-        _60min()
+        fourth_Time()
     }
 })
